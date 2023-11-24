b1a9608d1f744e1fc86880cf3817eaf03ce5d224
Folksykarly <karlaflrs_31@hotmail.com>
2023-11-23 23:46:44 -0600

    ya se puede guardar y mostrar datos de usuarios generales y de trabajadores por separado

--- comun/sql/tabla.sql
+++
@@ -1,7 +0,0 @@
-create table users_guards(
-    			num int AUTO_INCREMENT PRIMARY KEY,
-				id numeric,
-				nombre varchar(20),
-    			apellido varchar(20),
-                correo varchar(40)
-				);
